a2112799f9ce7616e458bd27f7b9adba8b461201
Max <101049443+Shvets-Maxx@users.noreply.github.com>
2024-12-02 17:41:08 +0200

    add token validation

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import Login from './components/Loginn/Login.js'
 import Registration from './components/Registration/Registration'
 import MainPage from './components/MainPage/MainPage';
 import {Routes , Route , Link} from 'react-router-dom'
+import TokenValid from './components/Loginn/TokenValid.js';
+
 
 
 function App() {
+
+  
   return (
     <div className="App">
+      <TokenValid/>
       <Routes>
         <Route path='/' element={<MainPage/>}/>
         <Route path='/registration' element={<Registration/>}/>
